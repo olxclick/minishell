ace4109a389d300c3db103618acb7a414e8d6137
olxclick <joaolazaro720@gmail.com>
2023-12-27 16:00:22 +0000

    small changes

--- src/expander_utils.c
+++ src/expander_utils.c
@@ -6,43 +6,54 @@
 /*   By: jbranco- <jbranco-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 11:47:40 by jbranco-          #+#    #+#             */
-/*   Updated: 2023/12/21 17:19:27 by jbranco-         ###   ########.fr       */
+/*   Updated: 2023/12/27 15:59:44 by jbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+char	*get_input_unquoted(char *input, char c, int i, char *new_input)
+{
+	char	*buff;
+	int		end;
+
+	end = ft_strlen(input) - 1;
+	while (input[end] != c)
+		end--;
+	while (input[end] == c)
+		end--;
+	buff = ft_substr(input, i, end - i + 1);
+	end++;
+	while (input[end] == c)
+		end++;
+	new_input = ft_strjoin(buff, &input[end]);
+	free_buf(buff, input);
+	return (new_input);
+}
+
 char	*remove_quotes(char *input)
 {
 	int		i;
 	char	*new_input;
-	char	*buff;
 	char	c;
 	int		end;
 
 	i = 0;
 	c = '\0';
+	end = ft_strlen(input) - 1;
+	new_input = NULL;
 	if (input[i] == SINGLE_QUOTE[0] || input[i] == DOUBLE_QUOTE[0])
 		c = input[i];
-	end = ft_strlen(input) - 1;
 	while (input[i] && input[i] == c)
 		i++;
 	if (input[end] != c)
-	{
-		while (input[end] != c)
-			end--;
-		buff = ft_substr(input, i, end - 1);
-		new_input = ft_strjoin(buff, &input[end + 1]);
-		free(buff);
-		free(input);
-		return (new_input);
-	}
+		return (get_input_unquoted(input, c, i, new_input));
 	else
 	{
-		while (end > i && input[end] == c)
+		while (end >= i && input[end] == c)
 			end--;
 	}
-	new_input = ft_substr(input, i, end);
+	new_input = ft_substr(input, i, end - i + 1);
 	free(input);
 	return (new_input);
 }
@@ -82,7 +93,7 @@ char	*expand_var(char *input, t_envs *envs, int x, char *res)
 
 	pos = pos_env_var(envs, &input[x]);
 	if (pos < 0)
-	{	
+	{
 		if (res)
 		{
 			final = ft_strdup(res);
